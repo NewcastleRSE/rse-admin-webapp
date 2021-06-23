9b843fba921cc089386e2ba69af6932dfe08935c
maxatthemax <b9016183@newcastle.ac.uk>
2021-06-23 17:40:07 +0100

    added axios

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,16 @@
 <template>
   <h1>Login</h1>
   <router-link to="/">go to dashboard</router-link>
+  <button class="btn" @click="login">CLick here</button>
 </template>
 
 <script>
 export default {
   name: "login",
   methods: {
-    login() {},
+    login() {
+      console.log("click");
+    },
   },
 };
 </script>
