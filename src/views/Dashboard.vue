bbb349d06125535a26e93c41e694788ddd332e4a
maxatthemax <b9016183@newcastle.ac.uk>
2021-06-24 15:29:50 +0100

    working auth get demo

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,7 +4,6 @@
   <br />
   <a href="http://localhost:1337/connect/microsoft">login</a>
   <p>{{ members }}</p>
-  <p>{{ token }}</p>
 </template>
 
 <script>
@@ -16,29 +15,48 @@ export default {
     return {
       token: "No Token",
       members: "No memebers",
+      jwt: "",
     };
   },
   created() {
     let url = window.location;
     this.token = new URLSearchParams(url.search).get("access_token");
     console.log(this.token);
+    if (this.token) {
+      this.getToken();
+    }
   },
   methods: {
+    getToken() {
+      const url =
+        "http://localhost:1337/auth/microsoft/callback/?access_token=" +
+        this.token;
+      axios
+        .get(url)
+        .then((res) => {
+          this.jwt = res.data.jwt;
+          console.log(this.jwt);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
     async getMembers() {
       console.log("click");
-      const token = this.token;
 
       axios
         .get("http://localhost:1337/members", {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${this.jwt}`,
           },
         })
         .then((response) => {
           console.log(response.data);
+          this.members = response.data;
         })
         .catch((error) => {
           console.log(error);
+          this.members = error;
         });
     },
   },
