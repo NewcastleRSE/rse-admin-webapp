0c653834f3b839a0c21a624e0a09c9b7f2798faa
maxatthemax <b9016183@newcastle.ac.uk>
2021-06-28 16:29:38 +0100

    updated token handling in router to use store rather than localStorage

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import jwt_decode from "jwt-decode";
+import store from "../store";
 
 import Login from "../views/Login";
 import Dashboard from "../views/Dashboard";
@@ -11,55 +12,46 @@ import Dashboard from "../views/Dashboard";
  * @return true : if the token is not expired
  */
 function isTokenValid() {
-    var token = localStorage.getItem("jwt");
-    if (token) {
-        var decoded = jwt_decode(token);
-        var exp = decoded.exp;
-        console.log("here " + decoded.exp);
-        if (Date.now() < exp * 1000) {
-            return true; // when token is expired
-        }
+  var token = store.state.auth.jwt;
+  if (token) {
+    var decoded = jwt_decode(token);
+    var exp = decoded.exp;
+    if (Date.now() < exp * 1000) {
+      return true; // when token is valid
     }
-    return false; // when token is not expired
+  }
+  return false; // when token is not valid
 }
 
-
-
-const routes = [{
-        path: "/",
-        redirect: {
-            name: "Login",
-        },
+const routes = [
+  {
+    path: "/",
+    redirect: {
+      name: "Login",
     },
-    {
-        path: "/login",
-        name: "Login",
-        component: Login,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    beforeEnter: (to, from, next) => {
+      if (!isTokenValid()) {
+        next("/login");
+      } else {
+        next();
+      }
     },
-    {
-        path: "/dashboard",
-        name: "Dashboard",
-        component: Dashboard,
-        beforeEnter: (to, from, next) => {
-            //console.log(this.$store.state.auth);
-            //if (this.$store.state.auth == false) {
-            //  next("/login");
-            //} else {
-            //  next();
-            //}
-            if (!isTokenValid()) { // if token is expired or jwt is null
-                // might need ===
-                next("/login");
-            } else {
-                next();
-            }
-        },
-    },
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
