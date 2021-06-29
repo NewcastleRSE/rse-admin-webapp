b9e84ae1ce4e6ac2221701d140c1e0ec705c73f0
maxatthemax <b9016183@newcastle.ac.uk>
2021-06-29 11:11:08 +0100

    added assignments and project getters to store

--- src/store/modules/getters.js
+++ src/store/modules/getters.js
@@ -5,6 +5,8 @@ export default {
   namespaced: true,
   state: {
     members: [],
+    projects: [],
+    assignments: [],
   },
 
   getters: {},
@@ -13,6 +15,12 @@ export default {
     //sync, updates state
     getMembers(state, members) {
       state.members = members;
+    },
+    getProjects(state, projects) {
+      state.projects = projects;
+    },
+    getAssignments(state, assignments) {
+      state.assignments = assignments;
     },
   },
 
@@ -28,6 +36,36 @@ export default {
         .then((response) => {
           console.log(response.data);
           commit("getMembers", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getProjects({ commit, rootState }) {
+      axios
+        .get("http://localhost:1337/projects", {
+          headers: {
+            Authorization: `Bearer ${rootState.auth.jwt}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          commit("getProjects", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getAssignments({ commit, rootState }) {
+      axios
+        .get("http://localhost:1337/assignments", {
+          headers: {
+            Authorization: `Bearer ${rootState.auth.jwt}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          commit("getAssignments", response.data);
         })
         .catch((error) => {
           console.log(error);
