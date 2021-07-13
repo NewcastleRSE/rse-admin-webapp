cab9789f9ef9b6f3589ec5efff0a2fbb982e0a9a
runnily <32201807+runnily@users.noreply.github.com>
2021-07-13 05:48:09 +0100

    added listed of unassigned prjs and remove and add btn functionailty for the grant chart

--- src/main.js
+++ src/main.js
@@ -5,15 +5,13 @@ import store from "./store";
 import Highcharts from "highcharts";
 import Draggable from "highcharts/modules/draggable-points";
 import Gantt from "highcharts/modules/gantt";
-//import Stock from "highcharts/modules/stock";
 import HighchartsVue from "highcharts-vue";
 
 Gantt(Highcharts);
 Draggable(Highcharts);
-//Stock(Highcharts);
 
 createApp(App)
-  .use(store)
-  .use(router)
-  .use(HighchartsVue)
-  .mount("#app");
+    .use(store)
+    .use(router)
+    .use(HighchartsVue)
+    .mount("#app");
--- src/views/Assignments.vue
+++ src/views/Assignments.vue
@@ -2,6 +2,7 @@
   <h1>Assignments</h1>
   
    <div>
+    <button @click="remove()" class="length">Remove</button>
    <highcharts
       :constructorType="'ganttChart'"
       class="hc"
@@ -9,35 +10,122 @@
       ref="chart"
     ></highcharts>
     </div>
+    <div id="this" v-html="unallocationPrjs">
+    </div>
+    <div>
+      <form v-on:submit.prevent="onSubmit" id = "dialogue" class = "openDialogue">
+        <label for="prjName">name </label><br>
+        <input type="text" id="prjName" name="prjName" v-model="prjName" disabled><br>
+
+        <label for="start">Start date</label><br>
+        <input type="date" id="start" name="start" v-model="start"><br>
+
+        <label for="end">End date</label><br>
+        <input type="date" id="end" name="end" v-model="end"><br>
+
+        <br>
+        <select id="select" v-model="user">
+          <option disabled value="">Please select one</option>
+          <option v-for="member in members" v-bind:key="member">{{member}}</option>
+        </select>
+       
+        <br>
+
+        <input type="submit" value="Add" @click="addPrj()">
+        <input type="button" value="Cancel" @click="cancel()">
+
+    </form> 
+    </div>
 </template>
 
 
 <script>
-//import axios from "axios";
+
 
 export default {
   name: "dashboard",
-  data() {
-    console.log("here");
-    var assignments = this.$store.state.getters.assignments
-    var members = [];
-    //var dictAssignments = [];
-    assignments.forEach( (assign) => {
-      members.push(assign.member.firstname + " " + assign.member.surname)}
+
+  methods: {
+    /*
+    * unassignedPrjBtns: This will be used to render a list of buttons to allow
+    *                    allocations of unassigned projects.
+    */
+    unassignedPrjBtns() {
+      var btns = "<h2> Unassigned Projects <h2>";
+      this.$store.dispatch("get/getProjects", ["awaitingAllocation"]).then(() => {
+      var projects = this.$store.getters["get/getProjects"];
+      console.log(projects);
+      projects.forEach( (project) => {
+        btns  += `<button id='${project.name}' class='btn'> ${project.name} </button>`;
+      })
+      console.log(btns);
+      this.unallocationPrjs =  `<div> ${btns} </div>`;
+      return this.unallocationPrjs;
+    })
     
-    )
-   
+    },
+
+    /*
+    * addPrj(): This will be used to add project onto grantt chart
+    * TODO: 
+    * Do not allow the same form to submit twices
+    * Update assigned project to database
+    * 
+    */
+    addPrj() {
+      var start = this.start.split("-");
+      var end = this.end.split("-");
+      var chart = this.chartOptions.series[0].data;
+      chart.push(
+        {name: this.user,
+        start: Date.UTC(start[0], start[1], start[2]),
+        end: Date.UTC(end[0], end[1], end[2]),
+        })
+      document.getElementById("dialogue").style.visibility = "hidden";
+      alert("Added!");
+    },
+
+    /*
+    * cancel(): This will be used to cancel the model
+    */
+    cancel() {
+      var dialogue  = document.getElementById("dialogue");
+      dialogue.style.visibility = "hidden";
+    },
+
+    /**
+     * remove(): This will be used to remove a project from grantt chart
+     */
+    remove() {
+      var points = this.$refs.chart.chart.getSelectedPoints();
+      points.forEach((point) => {
+        point.remove();
+      })
+      alert("Removed!")
+    },
+  },
+
+  data() {
+
+    
+  
     return {
+      
+      unallocationPrjs: this.unassignedPrjBtns(),
+
+      prjName: "",
+
+      start: null,
+
+      end: null,
+
+      members: [],
+
+      user: null,
+
       chartOptions: {
         title: {
           text: "Gantt Chart with Progress Indicators",
-        },
-
-        yAxis: {
-        type: 'category',
-        categories: members,
-        min: 0,
-        max: members.length - 1
         },
 
         xAxis: {
@@ -45,52 +133,96 @@ export default {
         },
 
         plotOptions: {
-          series: {
+        series: {
+            animation: false, // Do not animate dependency connectors
             dragDrop: {
-              draggableX: true,
+                draggableX: true,
+                draggableY: true,
+            
             },
+            /*dataLabels: {
+                enabled: true,
+                format: '{point.name}',
+                style: {
+                    cursor: 'default',
+                    pointerEvents: 'none'
+                }
+            },*/
+            allowPointSelect: true,
+           
+            }
+        },
+
+        navigator: {
+          enabled: true,
+          liveRedraw: true,
+          series: {
+            type: "gantt",
+            pointPlacement: 0.5,
+            pointPadding: 0.25,
           },
+          yAxis: {
+            reversed: true,
+            categories: [],
+          },
+        },
+        scrollbar: {
+          enabled: true,
+        },
+        rangeSelector: {
+          enabled: true,
+          selected: 0,
         },
 
         series: [
           {
             name: "Project 1",
-            data: [
-              {
-                name: "Start prototype",
-                start: Date.UTC(2014, 10, 18),
-                end: Date.UTC(2014, 10, 25),
-                completed: 0.25,
-              },
-              {
-                name: "Test prototype",
-                start: Date.UTC(2014, 10, 27),
-                end: Date.UTC(2014, 10, 29),
-              },
-              {
-                name: "Develop",
-                start: Date.UTC(2014, 10, 20),
-                end: Date.UTC(2014, 10, 25),
-                completed: {
-                  amount: 0.12,
-                  fill: "#fa0",
-                },
-              },
-              {
-                name: "Run acceptance tests",
-                start: Date.UTC(2014, 10, 23),
-                end: Date.UTC(2014, 10, 26),
-              },
-            ],
+            data: null
           },
         ],
       },
     };
   },
+  
   created() {
-    this.$store.dispatch("getters/getMembers", "1");
-    this.$store.dispatch("getters/getAssignments");
+    this.$store.dispatch("get/getAssignments");
+    var assignments = this.$store.state.get.assignments;
+    var members = [];
+    var dictAssignments = [];
+    assignments.forEach( (assign) => {
+      var startDate = new Date(assign.startDate);
+      var endDate = new Date(assign.endDate);
+      dictAssignments.push({
+          start: Date.UTC(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDate()),
+          end: Date.UTC(endDate.getUTCFullYear(), endDate.getUTCMonth(), endDate.getUTCDate()),
+          name: assign.member.firstname + " " + assign.member.surname,
+        
+      });
+      members.push(assign.member.firstname + " " + assign.member.surname);
+      })
+      this.chartOptions.series[0].data = dictAssignments
+      this.members = members;    
   },
-  methods: {},
+
+  updated() {
+    var dialogue  = document.getElementById("dialogue");
+    var btns = document.getElementsByClassName('btn');
+    console.log(btns.length)
+    btns.forEach( (btn) => {
+      btn.addEventListener("click", function() {
+        dialogue.style.visibility = "visible";
+      })
+    })
+ 
+  },
+
 };
 </script>
+
+<style>
+.openDialogue {
+  background-color: gray;
+  visibility: hidden;
+  width: 50%;
+}
+</style>
