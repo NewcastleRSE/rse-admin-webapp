c6c8e0da211f5151ee4087a6d093143aaa064f48
runnily <32201807+runnily@users.noreply.github.com>
2021-07-19 15:57:15 +0100

    add project cards

--- src/components/Assignments/ProjectsCard.vue
+++ src/components/Assignments/ProjectsCard.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="panel">
+  <div class="panel" style="overflow: scroll">
     <h3 class="panel-heading">Projects Card</h3>
+    <p class="panel-tabs">
+      <a class="is-active" @click="filter()">All</a>
+      <a @click="filter('Funded Awaiting Allocation')">Unallocated</a>
+      <a @click="filter('Allocated')">Allocated</a>
+    </p>
     <ul>
-      <li v-if="projects">
+      <li v-if="filterProjects">
         <a
-          v-for="project in projects"
+          v-for="project in filterProjects"
           v-bind:key="project"
           @click="toggleModal(project)"
-          class="panel-block"
-        >
+          class="panel-block">
           {{ project.name }}
         </a>
       </li>
@@ -16,7 +20,6 @@
     </ul>
   </div>
 </template>
-
 <script>
 export default {
   name: "ProjectsCard",
@@ -24,29 +27,47 @@ export default {
     return {
       projects: null,
       members: [],
+      filterProjects: null, // select allocated
     };
   },
-
   methods: {
     toggleModal(project) {
       this.$emit("toggleModal", project);
     },
+    filter(name = null) {
+      this.$store.dispatch("projects/getProjects");
+      this.filterProjects = [];
+      let projects = this.$store.getters["projects/getProjects"];
+      if (name) {
+        for (let idx in projects) {
+          let project = projects[idx];
+          if (project.stage === name) {
+            this.filterProjects.push(project);
+          }
+        }
+      } else {
+        this.filterProjects = projects
+      }
+      console.log(this.filterProjects);
+      console.log(projects);
+    },
   },
-
   created() {
-    this.projects = this.getProjects;
+    let projects = this.getProjects;
+    this.projects = projects;
+    this.filterProjects = projects;
   },
-
   computed: {
     getProjects() {
       // gets updated value from store
-      return this.$store.getters["get/getProjects"];
+      return this.$store.getters["get/getProjects"]; // even when projects is identical to get, dont work
     },
   },
   watch: {
     getProjects(update) {
       // watches 'getProjects()' to update data in chart
       this.projects = update;
+      this.filterProjects = update;
     },
   },
 };
