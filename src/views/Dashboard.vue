1381f6ef427b6716e0a97e5bb5182afd921e6484
markdturner <mark.turner@newcastle.ac.uk>
2021-12-23 14:24:26 +0000

    replaced old dashboard layout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,34 +2,34 @@
   <div>
     <div class="flex flex-wrap">
       <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-        <card-line-chart />
+        <card-projects-overview />
       </div>
       <div class="w-full xl:w-4/12 px-4">
-        <card-bar-chart />
+        <card-cost-recovery-overview />
       </div>
     </div>
     <div class="flex flex-wrap mt-4">
       <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-        <card-page-visits />
+        <card-projects-status />
       </div>
       <div class="w-full xl:w-4/12 px-4">
-        <card-social-traffic />
+        <card-time-split />
       </div>
     </div>
   </div>
 </template>
 <script>
-import CardLineChart from "@/components/Cards/CardLineChart.vue";
-import CardBarChart from "@/components/Cards/CardBarChart.vue";
-import CardPageVisits from "@/components/Cards/CardPageVisits.vue";
-import CardSocialTraffic from "@/components/Cards/CardSocialTraffic.vue";
+import CardCostRecoveryOverview from "@/components/Cards/CardCostRecoveryOverview.vue";
+import CardProjectsOverview from "@/components/Cards/CardProjectsOverview.vue";
+import CardProjectsStatus from "@/components/Cards/CardProjectsStatus.vue";
+import CardTimeSplit from "@/components/Cards/CardTimeSplitOverview.vue";
 export default {
   name: "dashboard-page",
   components: {
-    CardLineChart,
-    CardBarChart,
-    CardPageVisits,
-    CardSocialTraffic,
+    CardCostRecoveryOverview,
+    CardProjectsOverview,
+    CardProjectsStatus,
+    CardTimeSplit,
   },
 };
 </script>
