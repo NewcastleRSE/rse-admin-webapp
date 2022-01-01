de4d79c6b2c1ed3018b1883fac3f7c2725705f6a
markdturner <mark.turner@newcastle.ac.uk>
2022-01-01 20:58:15 +0000

    installed and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false,
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        amber: colors.amber,
+        black: colors.black,
+        blue: colors.blue,
+        cyan: colors.cyan,
+        emerald: colors.emerald,
+        fuchsia: colors.fuchsia,
+        gray: colors.trueGray,
+        blueGray: colors.blueGray,
+        coolGray: colors.coolGray,
+        trueGray: colors.trueGray,
+        warmGray: colors.warmGray,
+        green: colors.green,
+        indigo: colors.indigo,
+        lime: colors.lime,
+        orange: colors.orange,
+        pink: colors.pink,
+        purple: colors.purple,
+        red: colors.red,
+        rose: colors.rose,
+        sky: colors.sky,
+        teal: colors.teal,
+        violet: colors.violet,
+        yellow: colors.amber,
+        white: colors.white,
+      },
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
