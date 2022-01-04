b70575278339937164e2e658dd5d320343f738f2
markdturner <mark.turner@newcastle.ac.uk>
2022-01-04 20:49:20 +0000

    changed project input icon

--- src/components/Assignments/CreateModal.vue
+++ src/components/Assignments/CreateModal.vue
@@ -39,7 +39,7 @@
 
             <div class="relative flex w-full flex-wrap items-stretch mb-3">
               <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i class="fas fa-lock"></i>
+                <i class="fas fa-briefcase"></i>
               </span>
               <input type="text" placeholder="Project" class="py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full pl-10"/>
             </div>
