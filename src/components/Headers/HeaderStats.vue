6310f486307acc748a218277e459fe2f51e3c326
markdturner <mark.turner@newcastle.ac.uk>
2022-04-06 21:17:02 +0100

    moved availability calculation to vuex store

--- src/components/Headers/HeaderStats.vue
+++ src/components/Headers/HeaderStats.vue
@@ -7,37 +7,37 @@
         <div class="flex flex-wrap">
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <availability-card
-              :avatar=getAvatar(nextMiddleware.name)
+              :avatar=getAvatar(nextRSE)
               team="ALL"
-              :name=nextMiddleware.name
-              :from=nextMiddleware.from
-              :waitTime=nextMiddleware.wait  
+              :name="nextRSE.firstname + ' ' + nextRSE.lastname"
+              :from=formatDate(nextRSE.nextAvailableDate)
+              :waitTime=nextRSE.wait  
             />
           </div>
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <availability-card
-              :avatar=getAvatar(nextMiddleware.name)
+              :avatar=getAvatar(nextMiddleware)
               team="MIDDLEWARE"
-              :name=nextMiddleware.name
-              :from=nextMiddleware.from
+              :name="nextMiddleware.firstname + ' ' + nextMiddleware.lastname"
+              :from=formatDate(nextMiddleware.nextAvailableDate)
               :waitTime=nextMiddleware.wait              
             />
           </div>
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <availability-card
-              :avatar=getAvatar(nextWebMobile.name)
+              :avatar=getAvatar(nextWebMobile)
               team="WEB &amp; MOBILE"
-              :name=nextWebMobile.name
-              :from=nextWebMobile.from
+              :name="nextWebMobile.firstname + ' ' + nextWebMobile.lastname"
+              :from=formatDate(nextWebMobile.nextAvailableDate)
               :waitTime=nextWebMobile.wait  
             />
           </div>
           <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
             <availability-card
-              :avatar=getAvatar(nextDataScience.name)
+              :avatar=getAvatar(nextDataScience)
               team="DATA SCIENCE"
-              :name=nextDataScience.name
-              :from=nextDataScience.from
+              :name="nextDataScience.firstname + ' ' + nextDataScience.lastname"
+              :from=formatDate(nextDataScience.nextAvailableDate)
               :waitTime=nextDataScience.wait 
             />
           </div>
@@ -49,66 +49,32 @@
 
 <script>
 import AvailabilityCard from "@/components/Cards/Availability.vue";
-
-function weeksBetween(d1, d2) {
-    return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
-}
-
-function nextAvailable(teams, teamName) {
-  // console.log(teams, teamName)
-  var mostRecentDate = new Date(Math.min.apply(null, teams[teamName].map( e => {
-      if(e.availability.from) {
-        return e.availability.from;
-      } else {
-        // return a date 100 years in the future to avoid 1970 being min date
-        var today = new Date();
-        var year = today.getFullYear();
-        var month = today.getMonth();
-        var day = today.getDate();
-        return new Date(year + 100, month, day);
-      }
-      
-    })));
-    let member = teams[teamName].filter( e => {
-        return new Date(e.availability.from).getTime() === mostRecentDate.getTime();
-    })[0];
-
-    return {
-      from: member.availability.from.toLocaleDateString(),
-      to: member.availability.to ? member.availability.to.toLocaleDateString() : null,
-      FTE: member.availability.FTE,
-      wait: weeksBetween(new Date(), member.availability.from) + " Weeks",
-      name: member.firstname + ' ' + member.lastname,
-    }
-}
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
     AvailabilityCard,
   },
   data() {
-
-    const teams = this.$store.getters["members/getFullMembers"].reduce(function (r, a) {
-        r[a.team] = r[a.team] || [];
-        r[a.team].push(a);
-        return r;
-    }, Object.create(null));
-
     return {
-      nextDataScience: nextAvailable(teams, 'DataScience'),
-      nextMiddleware: nextAvailable(teams, 'Middleware'),
-      nextWebMobile: nextAvailable(teams, 'WebMobile'),
       avatars: [],
       getAvatar: (rse) => {
+          let name = rse.firstname + ' ' + rse.lastname
           let avatar = this.avatars.find((avatar) => {
-            return avatar.name === rse.toLowerCase()
+            return avatar.name === name.toLowerCase()
           })
           return avatar ? avatar.pathLong : '' 
       },
+      formatDate: (date) => {
+        return new Date(date).toLocaleDateString()
+      }
     };
   },
   mounted() {
     this.importAvatars(require.context('@/assets/img/avatars/', true, /\.(gif|jpe?g|tiff?|png|webp|bmp)$/));
+  },
+  computed: {
+    ...mapGetters('rses', ['nextRSE', 'nextWebMobile', 'nextMiddleware', 'nextDataScience'])
   },
   methods: {
     importAvatars(r) {
---
+++ src/store/modules/rses.js
@@ -0,0 +1,117 @@
+//import router from "../../router";
+import axios from "axios";
+import { DateTime } from "luxon";
+
+export default {
+    namespaced: true,
+
+    /*
+    Global Variables
+    Call state with $store.state.{module}.{stateName}
+    */
+    state: {
+        rses: [],
+    },
+
+    getters: {
+        getRses: (state) => {
+            state.rses.sort(function(a, b) {
+                return a.lastname.localeCompare(b.lastname);
+            });
+
+            const rses = state.rses.map((rse) => {
+                const ganttItem = {};
+
+                ganttItem.id = rse.id.toString();
+                ganttItem.name = rse.firstname + " " + rse.lastname;
+                ganttItem.team = rse.Team;
+                ganttItem.email = rse.email;
+                ganttItem.collapsed = true;
+
+                return ganttItem;
+            });
+
+            return rses;
+        },
+        getFullRses: (state) => {
+          state.rses.sort(function(a, b) {
+              return a.lastname.localeCompare(b.lastname);
+          });
+        },
+        nextRSE: (state) => {
+          let fullTeam = state.rses
+          fullTeam.sort((a, b) => { return new Date(a.nextAvailableDate) - new Date(b.nextAvailableDate) } )
+          
+          let nextRSE = fullTeam[0],
+              wait = DateTime.now().diff(DateTime.fromISO(nextRSE.nextAvailableDate), ['months', 'days']).toObject()
+
+          nextRSE.wait = `${wait.months} Months ${Math.floor(wait.days)} Days`
+          
+          return nextRSE
+        },
+        nextWebMobile: (state) => {
+          let webMobileTeam = state.rses.filter(rse => rse.team === 'WebMobile')
+          webMobileTeam.sort((a, b) => { return new Date(a.nextAvailableDate) - new Date(b.nextAvailableDate) } )
+          
+          let nextRSE = webMobileTeam[0],
+          wait = DateTime.now().diff(DateTime.fromISO(nextRSE.nextAvailableDate), ['months', 'days']).toObject()
+
+          nextRSE.wait = `${wait.months} Months ${Math.floor(wait.days)} Days`
+          
+          return nextRSE
+        },
+        nextMiddleware: (state) => {
+          let middlewareTeam = state.rses.filter(rse => rse.team === 'Middleware')
+          middlewareTeam.sort((a, b) => { return new Date(a.nextAvailableDate) - new Date(b.nextAvailableDate) } )
+          
+          let nextRSE = middlewareTeam[0],
+          wait = DateTime.now().diff(DateTime.fromISO(nextRSE.nextAvailableDate), ['months', 'days']).toObject()
+
+          nextRSE.wait = `${wait.months} Months ${Math.floor(wait.days)} Days`
+          
+          return nextRSE
+        },
+        nextDataScience: (state) => {
+          let dataScienceTeam = state.rses.filter(rse => rse.team === 'DataScience')
+          dataScienceTeam.sort((a, b) => { return new Date(a.nextAvailableDate) - new Date(b.nextAvailableDate) } )
+          
+          let nextRSE = dataScienceTeam[0],
+          wait = DateTime.now().diff(DateTime.fromISO(nextRSE.nextAvailableDate), ['months', 'days']).toObject()
+
+          nextRSE.wait = `${wait.months} Months ${Math.floor(wait.days)} Days`
+          
+          return nextRSE
+        }
+    },
+
+    mutations: {
+        //sync, updates state
+        getRses(state, rses) {
+          state.rses = rses;
+        },
+    },
+
+    actions: {
+        //async, commits mutations
+
+    /*
+    Gets rse or rses from DB
+    Call with this.$store.dispatch("rses/getRses", "{id}");
+    Can leave parameter empty and will call all rses
+    */
+    getRses({ commit, rootState }, id = "") {
+      axios
+        .get(`${process.env.VUE_APP_API_URL}/rses/${id}`, {
+          headers: {
+            Authorization: `Bearer ${rootState.auth.jwt}`,
+          },
+        })
+        .then((response) => {
+          commit("getRses", response.data.data );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+}
