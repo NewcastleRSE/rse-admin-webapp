bd5805a22c99c588501c6adce223a460530b094d
markdturner <mark.turner@newcastle.ac.uk>
2022-04-07 09:19:07 +0100

    fetch profile photo as base64

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -12,9 +12,10 @@ export default {
   mutations: {
     //sync, updates state
     login(state, data) {
-      state.accessToken = data.accessToken;
-      state.jwt = data.jwt;
-      state.user = data.profile;
+      state.accessToken = data.accessToken
+      state.jwt = data.jwt
+      state.user = data.profile
+      state.user.photo = data.photo
       this.dispatch("projects/getProjects")
       this.dispatch("capacity/getCapacity")
       this.dispatch("rses/getRses")
@@ -33,16 +34,18 @@ export default {
     login({ commit }, accessToken) {
       const loginUrl = process.env.VUE_APP_API_URL + "/auth/microsoft/callback/?access_token=" + accessToken;
       const azureConfig = { headers: { Authorization: `Bearer ${accessToken}` }};
-    
+
       let fetchJWT = axios.get(loginUrl)
       let fetchProfile = axios.get('https://graph.microsoft.com/v1.0/me', azureConfig)
+      let fetchPhoto = axios.get('https://graph.microsoft.com/v1.0/me/photo/$value', {...azureConfig, responseType: 'arraybuffer'})
 
-      Promise.all([fetchJWT, fetchProfile])
+      Promise.all([fetchJWT, fetchProfile, fetchPhoto])
       .then((values) => {
           commit("login", {
             accessToken: accessToken,
             jwt: values[0].data.jwt,
-            profile: values[1].data
+            profile: values[1].data,
+            photo: Buffer.from(values[2].data, 'binary').toString('base64')
           })
       })
       .catch((err) => {
