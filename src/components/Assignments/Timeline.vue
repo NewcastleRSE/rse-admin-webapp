79de0028016869dcd15143efc8a98743c1aa5250
markdturner <mark.turner@newcastle.ac.uk>
2022-07-24 14:18:37 +0100

    expandable rows to show assignments

--- src/components/Assignments/Timeline.vue
+++ src/components/Assignments/Timeline.vue
@@ -15,46 +15,57 @@ import { Plugin as Selection } from "gantt-schedule-timeline-calendar/dist/plugi
 import { Plugin as ItemResizing } from "gantt-schedule-timeline-calendar/dist/plugins/item-resizing.esm.min.js";
 import { Plugin as ItemMovement } from "gantt-schedule-timeline-calendar/dist/plugins/item-movement.esm.min.js";
 import { Plugin as Bookmarks } from "gantt-schedule-timeline-calendar/dist/plugins/time-bookmarks.esm.min.js";
+import { Plugin as HighlightWeekends } from 'gantt-schedule-timeline-calendar/dist/plugins/highlight-weekends.esm.min.js';
+import { Plugin as CalendarScroll } from 'gantt-schedule-timeline-calendar/dist/plugins/calendar-scroll.esm.min.js';
+
 import "gantt-schedule-timeline-calendar/dist/style.css";
 import { ref, onMounted, onBeforeUnmount } from "vue";
+import { useStore } from 'vuex';
 
 var globalThis = require('globalthis')();
 
-// helper functions
-function generateRows() {
+function generateRows(RSEs) {
   /**
    * @type { import("gantt-schedule-timeline-calendar").Rows }
    */
   const rows = {};
-  for (let i = 0; i < 20; i++) {
-    const id = GSTC.api.GSTCID(i.toString());
+  RSEs.forEach(rse => {
+    const id = GSTC.api.GSTCID(rse.id),
+          assignments = GSTC.api.GSTCID(rse.id + '-assignments')
     rows[id] = {
       id,
-      label: `Row ${i}`,
+      label: rse.name,
     };
-  }
+    rows[assignments] = {
+      assignments,
+      label: '',
+      parentId: id,
+    };
+  })
   return rows;
 }
-function generateItems() {
+function generateAssignments(assignments) {
+
   /**
    * @type { import("gantt-schedule-timeline-calendar").Items }
    */
   const items = {};
-  let start = GSTC.api.date().startOf("day").subtract(6, "day");
-  for (let i = 0; i < 20; i++) {
-    const id = GSTC.api.GSTCID(i.toString());
-    const rowId = GSTC.api.GSTCID(Math.floor(Math.random() * 100).toString());
-    start = start.add(1, "day");
+  assignments.forEach(assignment => {
+    const id = GSTC.api.GSTCID(assignment.assignmentID),
+          rowId = GSTC.api.GSTCID(assignment.rse.id + '-assignments')
+ 
     items[id] = {
       id,
-      label: `Item ${i}`,
+      label: assignment.project.name,
       rowId,
       time: {
-        start: start.valueOf(),
-        end: start.add(1, "day").endOf("day").valueOf(),
+        start: GSTC.api.date(assignment.start),
+        end: GSTC.api.date(assignment.end),
       },
-    };
-  }
+    }
+
+  });
+
   return items;
 }
 // main component
@@ -64,38 +75,40 @@ export default {
     let gstc, state;
     const gstcElement = ref(null);
     onMounted(() => {
+
+      const store = useStore()
+
       /**
        * @type { import("gantt-schedule-timeline-calendar").Config }
        */
       const config = {
         licenseKey:
-        `${process.env.VUE_APP_GANTT_KEY}`,
-        plugins: [TimelinePointer(), Selection(), ItemResizing(), ItemMovement(), Bookmarks()],
+          `${process.env.VUE_APP_GANTT_KEY}`,
+        plugins: [HighlightWeekends(), TimelinePointer(), Selection(), ItemResizing(), ItemMovement(), Bookmarks(), CalendarScroll()],
         list: {
           columns: {
             data: {
-              [GSTC.api.GSTCID("id")]: {
-                id: GSTC.api.GSTCID("id"),
-                width: 60,
-                data: ({ row }) => GSTC.api.sourceID(row.id),
-                header: {
-                  content: "ID",
-                },
-              },
               [GSTC.api.GSTCID("label")]: {
                 id: GSTC.api.GSTCID("label"),
                 width: 200,
                 data: "label",
+                sortable: 'label',
+                expander: true,
                 header: {
-                  content: "Label",
+                  content: "Name",
                 },
               },
             },
           },
-          rows: generateRows(),
+          rows: generateRows(store.getters["rses/getRses"]),
         },
         chart: {
-          items: generateItems(),
+          items: generateAssignments(store.getters["assignments/getAssignments"]),
+          // time: {
+          //   from: GSTC.api.date("2020-01-01").valueOf(), // from 2020-01-01
+          //   to: GSTC.api.date("2020-06-01").endOf("month").valueOf(), // to 2020-06-31
+          //   calculatedZoomMode: true,
+          // }
         },
       };
       state = GSTC.api.stateFromConfig(config);
@@ -116,14 +129,14 @@ export default {
       });
     }
     function changeZoomLevel() {
-      state.update("config.chart.time.zoom", 21);
+      state.update("config.chart.time.zoom", 23);
     }
     return {
       gstcElement,
       updateFirstRow,
       changeZoomLevel,
     };
-  },
+  }
 };
 </script>
 <style scoped>
@@ -131,6 +144,7 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 .toolbox {
   padding: 10px;
 }
