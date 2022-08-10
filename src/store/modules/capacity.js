25cd96338ab0213817337498e9a3c49001538b5d
markdturner <mark.turner@newcastle.ac.uk>
2022-08-10 06:19:40 +0100

    calculate capacity and targets per month

--- src/store/modules/capacity.js
+++ src/store/modules/capacity.js
@@ -1,5 +1,6 @@
 //import router from "../../router";
-import axios from "axios";
+import axios from "axios"
+import { DateTime } from "luxon"
 
 export default {
     namespaced: true,
@@ -16,6 +17,43 @@ export default {
         getCapacity: (state) => {
             return state.capacity;
         },
+        getUtilisation: (state) => {
+          let utilisation = [],
+              startDate = DateTime.utc().startOf('month').minus({months: 12}),
+              endDate = DateTime.utc().startOf('month').plus({month: 13})
+
+          while(startDate.toISODate() !== endDate.toISODate()) {
+            
+            let capacities = state.capacity.filter(capacity => {
+              let capacityStart = DateTime.fromISO(capacity.start),
+                  capacityEnd = capacity.end ? DateTime.fromISO(capacity.end) : endDate
+
+              return startDate >= capacityStart && startDate <= capacityEnd
+            })
+            
+            let monthlyUtilisation = {
+              date: startDate.toISODate(),
+              capacityFTE: capacities.reduce(function (totalFTE, rse) { return totalFTE + rse.capacity }, 0),
+              targetFTE: Math.round(capacities.reduce(function (totalFTE, rse) { return totalFTE + rse.capacity }, 0) * 0.7),
+              actualFTE: 0,
+              capacityDays: capacities.reduce(function (totalFTE, rse) { return totalFTE + ((220/12) * (rse.capacity/100)) }, 0),
+              targetDays: Math.round(capacities.reduce(function (totalFTE, rse) { return totalFTE + ((220/12) * (rse.capacity/100)) }, 0) * 0.7),
+              actualDays: 0,
+            }
+
+            utilisation.push(monthlyUtilisation)
+
+            startDate = startDate.plus({months: 1})
+          }
+
+          console.log(utilisation)
+
+          // state.capacity.forEach(capacity => {
+          //   console.log(capacity)
+          // });
+
+          return utilisation
+        }
     },
 
     mutations: {
@@ -41,7 +79,7 @@ export default {
           },
         })
         .then((response) => {
-          commit("getCapacity", response.data);
+          commit("getCapacity", response.data.data);
         })
         .catch((error) => {
           console.log(error);
