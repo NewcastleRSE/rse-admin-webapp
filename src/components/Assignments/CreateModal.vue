86b269cf8b5a06dd959ae77c45955e3e6077800f
markdturner <mark.turner@newcastle.ac.uk>
2022-08-24 08:20:00 +0100

    Improved modal style

--- src/components/Assignments/CreateModal.vue
+++ src/components/Assignments/CreateModal.vue
@@ -1,71 +1,66 @@
 <template>
-    <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
-      <div class="relative w-auto my-6 mx-auto max-w-3xl">
-        <!--content-->
-        <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <!--header-->
-          <div class="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-            <h3 class="text-3xl font-semibold">
-              Create Assignment
-            </h3>
-            <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" v-on:click="toggleModal()">
-              <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                ×
-              </span>
-            </button>
-          </div>
-          <!--body-->
-          <div class="relative p-6 flex-auto">
-            <div class="relative flex w-full flex-wrap items-stretch mb-3">
-              <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i class="fas fa-user"></i>
-              </span>
-              <vue3-simple-typeahead placeholder="RSE" v-model="rse" :items=rses :minInputLength="1" :itemProjection="rseProjection" @selectItem="selectRse">
-              <template #list-item-text="slot">
-                <div class="align-middle whitespace-nowrap">
-                  <div class="flex items-center">
-                    <img :src="getAvatar(slot.itemProjection(slot.item))" class="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow mr-2" />
-                    <span v-html="slot.boldMatchText(slot.itemProjection(slot.item))"></span>
-                  </div>
+    <div v-if="showModal" tabindex="-1" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full h-screen md:inset-0 h-modal md:h-full">
+        <div class="flex flex-col justify-center relative p-4 w-full md:max-w-lg lg:max-w-4xl h-full mx-auto">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                        Create Assignment
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" v-on:click="toggleModal()">
+                        <i class="fa-solid fa-xmark"></i>
+                        <span class="sr-only">Close modal</span> 
+                    </button>
                 </div>
-              </template>
-              </vue3-simple-typeahead>
-            </div>
-            <div class="relative flex w-full flex-wrap items-stretch mb-3">
-              <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i class="fas fa-briefcase"></i>
-              </span>
-              <vue3-simple-typeahead placeholder="Project" v-model="project" :items=projects :minInputLength="1" :itemProjection="projectProjection" @selectItem="selectProject">
-              <template #list-item-text="slot">
-                <div class="align-middle whitespace-nowrap">
-                  <div class="flex items-center">
-                    <span v-html="slot.boldMatchText(slot.itemProjection(slot.item))"></span>
-                  </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <div class="relative flex w-full flex-wrap items-stretch mb-3">
+                      <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+                        <i class="fas fa-user"></i>
+                      </span>
+                      <vue3-simple-typeahead placeholder="RSE" v-model="rse" :items=rses :minInputLength="1" :itemProjection="rseProjection" @selectItem="selectRse">
+                      <template #list-item-text="slot">
+                        <div class="align-middle whitespace-nowrap">
+                          <div class="flex items-center">
+                            <img :src="getAvatar(slot.itemProjection(slot.item))" class="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow mr-2" />
+                            <span v-html="slot.boldMatchText(slot.itemProjection(slot.item))"></span>
+                          </div>
+                        </div>
+                      </template>
+                      </vue3-simple-typeahead>
+                    </div>
+                    <div class="relative flex w-full flex-wrap items-stretch mb-3">
+                      <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+                        <i class="fas fa-briefcase"></i>
+                      </span>
+                      <vue3-simple-typeahead placeholder="Project" v-model="project" :items=projects :minInputLength="1" :itemProjection="projectProjection" @selectItem="selectProject">
+                      <template #list-item-text="slot">
+                        <div class="align-middle whitespace-nowrap">
+                          <div class="flex items-center">
+                            <span v-html="slot.boldMatchText(slot.itemProjection(slot.item))"></span>
+                          </div>
+                        </div>
+                      </template>
+                      </vue3-simple-typeahead>
+                    </div>
+                    <div class="relative flex w-full flex-wrap items-stretch mb-3">
+                      <Datepicker v-model="dateRange" range :enableTimePicker="false" inputClassName="py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full pl-10" />
+                    </div>
+                    <div class="relative flex w-full flex-wrap items-stretch mb-3">
+                      <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+                        <i class="fas fa-percentage"></i>
+                      </span>
+                      <input v-model="split" type="number" min="0" max="100" step="1" placeholder="Split" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full pl-10"/>
+                    </div>
                 </div>
-              </template>
-              </vue3-simple-typeahead>
+                <!-- Modal footer -->
+                <div class="flex items-center justify-end p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                    <button v-on:click="toggleModal()" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                    <button v-on:click="addAssignment()" type="button" class="text-white bg-emerald-500 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
+                </div>
             </div>
-            <div class="relative flex w-full flex-wrap items-stretch mb-3">
-              <Datepicker v-model="dateRange" range :enableTimePicker="false" inputClassName="py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full pl-10" />
-            </div>
-            <div class="relative flex w-full flex-wrap items-stretch mb-3">
-              <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i class="fas fa-percentage"></i>
-              </span>
-              <input v-model="split" type="number" min="0" max="100" step="1" placeholder="Split" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:shadow-outline w-full pl-10"/>
-            </div>
-          </div>
-          <!--footer-->
-          <div class="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-            <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">
-              Close
-            </button>
-            <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="addAssignment()">
-              Create
-            </button>
-          </div>
         </div>
-      </div>
     </div>
     <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
 </template>
