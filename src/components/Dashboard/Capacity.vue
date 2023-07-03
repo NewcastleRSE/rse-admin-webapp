cc8eb563aa5cc181d896864a546d87ddf2efc01d
Mark Turner <mark.turner@ncl.ac.uk>
2023-07-03 21:29:12 +0100

    loaded capacity data from pinia store

---
+++ src/components/Dashboard/Capacity.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
+    <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
+      <div class="flex flex-wrap items-center">
+        <div class="relative w-full max-w-full flex-grow flex-1">
+          <h6 class="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
+            Monthly
+          </h6>
+          <h2 class="text-white text-xl font-semibold">
+            Utilisation
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="p-4 flex-auto">
+      <!-- Chart -->
+      <div class="relative h-350-px">
+        <canvas id="line-chart"></canvas>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { onMounted, defineOptions } from 'vue'
+import { useCapacitiesStore } from '@/stores/capacities'
+import Chart from 'chart.js/auto'
+import annotationPlugin from 'chartjs-plugin-annotation'
+import { DateTime } from 'luxon'
+
+defineOptions({
+  name: 'capacity'
+})
+
+Chart.register(annotationPlugin)
+
+onMounted(async () => {
+
+  const capacitiesStore = useCapacitiesStore()
+
+  let startDate = DateTime.utc().startOf('month').minus({months: 12}),
+      endDate = DateTime.utc().startOf('month').plus({month: 13})
+
+  let utilisationData = capacitiesStore.getUtilisation(startDate, endDate)
+
+  const labels = utilisationData.reduce(function (dates, month) {
+        let date = DateTime.fromISO(month.date)
+          return [...dates, date.toFormat('LLL yy')]
+        }, [])
+
+  let config = {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Target',
+          backgroundColor: "#38bdf8",
+          borderColor: "#38bdf8",
+          data: utilisationData.reduce(function (targets, month) { return [...targets, month.targetDays] }, []),
+          fill: false,
+          stepped: 'middle',
+        },
+        {
+          label: 'Capacity',
+          backgroundColor: "#0284c7",
+          borderColor: "#0284c7",
+          data: utilisationData.reduce(function (capacity, month) { return [...capacity, month.capacityDays] }, []),
+          fill: false,
+          stepped: 'middle',
+        },
+        {
+          label: 'Actual',
+          backgroundColor: "#f472b6",
+          borderColor: "#f472b6",
+          data: utilisationData.reduce(function (actual, month) { return [...actual, month.actualDays] }, []),
+          fill: false,
+          stepped: 'middle',
+        }
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      title: {
+        display: false,
+        text: "Utilisation",
+        fontColor: "white",
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: "#cbd5e1",
+          },
+          align: "end",
+          position: "bottom"
+        },
+        annotation: {
+          annotations: {
+            today: {
+              type: 'line',
+              xMin: DateTime.utc().toFormat('LLL yy'),
+              xMax: DateTime.utc().toFormat('LLL yy'),
+              borderColor: '#cbd5e1',
+              borderWidth: 1,
+              label: {
+                content: 'Today',
+                display: true,
+                position: 'end',
+                yAdjust: -6
+              }
+            }
+          }
+        }
+      },
+      tooltips: {
+        mode: "index",
+        intersect: false,
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true,
+      },
+      scales: {
+        x: {
+            ticks: {
+              color: "#cbd5e1",
+            },
+            display: true,
+            title: {
+              text: 'Month',
+              display: false,
+              color:"#cbd5e1"
+            },
+            gridLines: {
+              display: false,
+              borderDash: [2],
+              borderDashOffset: [2],
+              color: "#f1f5f9",
+              zeroLineColor: "rgba(0, 0, 0, 0)",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
+        },
+        y: {
+          min: 0,
+          ticks: {
+            color: "#cbd5e1",
+          },
+          display: true,
+          title: {
+            text: 'Days',
+            display: true,
+            color:"#cbd5e1"
+          },
+          gridLines: {
+            borderDash: [3],
+            borderDashOffset: [3],
+            drawBorder: false,
+            color: "rgba(255, 255, 255, 0.15)",
+            zeroLineColor: "rgba(33, 37, 41, 0)",
+            zeroLineBorderDash: [2],
+            zeroLineBorderDashOffset: [2],
+          },
+        },
+      },
+    },
+  }
+  var ctx = document.getElementById("line-chart").getContext("2d")
+  window.myLine = new Chart(ctx, config)
+})
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <div class="w-full -mt-24">
     <div class="flex flex-wrap">
       <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-        <!-- <card-projects-overview /> -->
+        <capacity />
       </div>
       <div class="w-full xl:w-4/12 px-4">
         <!-- <card-cost-recovery-overview /> -->
@@ -18,18 +18,19 @@
     </div>
   </div>
 </template>
-<script>
+<script setup>
+import Capacity from '@/components/Dashboard/Capacity.vue'
 // import CardCostRecoveryOverview from "@/components/Cards/CardCostRecoveryOverview.vue"
 // import CardProjectsOverview from "@/components/Cards/CardProjectsOverview.vue"
 // import CardProjectsStatus from "@/components/Cards/CardProjectsStatus.vue"
 // import CardTimeSplit from "@/components/Cards/CardTimeSplitOverview.vue"
-export default {
-  name: "dashboard-page",
-  // components: {
-  //   CardCostRecoveryOverview,
-  //   CardProjectsOverview,
-  //   CardProjectsStatus,
-  //   CardTimeSplit
-  // },
-}
+// export default {
+//   name: "dashboard-page",
+//   // components: {
+//   //   CardCostRecoveryOverview,
+//   //   CardProjectsOverview,
+//   //   CardProjectsStatus,
+//   //   CardTimeSplit
+//   // },
+// }
 </script>
