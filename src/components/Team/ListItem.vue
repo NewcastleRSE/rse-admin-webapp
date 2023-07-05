8834a128375b475070a87ab8194a563233826363
Mark Turner <mark.turner@ncl.ac.uk>
2023-07-05 19:55:51 +0100

    correct name position

--- src/components/Team/ListItem.vue
+++ src/components/Team/ListItem.vue
@@ -5,9 +5,8 @@
         <div class="min-w-0 flex-auto">
             <p class="text-sm font-semibold leading-6 text-gray-900">
             <a :href="`/team/${rse.id}`">
-                <span class="absolute inset-x-0 -top-px bottom-0">
+                <span class="absolute inset-x-0 -top-px bottom-0"></span>
                 {{ rse.displayName }}
-                </span>
             </a>
             </p>
             <p class="mt-1 flex text-xs leading-5 text-gray-500">
