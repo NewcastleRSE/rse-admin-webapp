cd87da8e6b94b8fedc83c082e9ed13549ab319b7
Mark Turner <mark.turner@ncl.ac.uk>
2023-08-08 13:39:44 +0100

    install svg loader

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import SimpleTypeahead from 'vue3-simple-typeahead'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@/assets/styles/index.css'
 
@@ -12,6 +11,5 @@ pinia.use(piniaPluginPersistedstate)
 
 createApp(App)
   .use(pinia)
-  .use(SimpleTypeahead)
   .use(router)
   .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import svgLoader from 'vite-svg-loader'
 const path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    viteCommonjs()
+    viteCommonjs(),
+    svgLoader()
   ],
   resolve: {
     alias: {
