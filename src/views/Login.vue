0e450258026f370f9f10cbe2a6d43b1e1ea3d154
Mark Turner <mark.turner@ncl.ac.uk>
2023-08-09 16:34:58 +0100

    fixed ncl logo on login screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-    <img alt="Newcastle University Logo" class="h-16 my-6" :src="nclLogo" />
+    <nclLogo class="my-6" />
     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">RSE Admin</h1>
     <p class="relative my-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">A web portal for the administration and project forecasting of the Newcastle University Research Software Engineering Team.</p>
     <a v-if="!accessToken" type="button" :href="apiUrl + '/connect/microsoft'" class="inline-flex items-center gap-x-2 rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
