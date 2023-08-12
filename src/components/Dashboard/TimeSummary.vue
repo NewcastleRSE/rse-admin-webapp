486beef03c93dd575e553cdb4e5121fdcc3f4804
Mark Turner <mark.turner@ncl.ac.uk>
2023-08-12 13:45:31 +0100

    remove rounding

--- src/components/Dashboard/TimeSummary.vue
+++ src/components/Dashboard/TimeSummary.vue
@@ -3,9 +3,9 @@
         <div class="flex-grow sm:block">
             <div class="relative" aria-hidden="true" v-if="rse.lastname !== 'Horsfall' && rse.lastname !== 'Lozada'">
                 <div class="overflow-hidden rounded-full bg-gray-200 h-4 relative">
-                    <div class="h-4 absolute rounded-l-full bg-cyan-600 left-0" :style="{ width: `${billable}%` }" />
+                    <div class="h-4 absolute bg-cyan-600 left-0" :style="{ width: `${billable}%` }" />
                     <div class="h-4 absolute bg-yellow-400" :style="{ left: `${billable}%`, width: `${nonBillable}%` }" />
-                    <div class="h-4 absolute rounded-r-full bg-red-400" :style="{ left: `${recorded}%`, width: `${missing}%` }" />
+                    <div class="h-4 absolute bg-red-400" :style="{ left: `${recorded}%`, width: `${missing}%` }" />
                 </div>
                 <div class="h-6 w-0.5 -top-1 absolute bg-black" :style="{ left: `${progressThroughCapacity}%` }" />
             </div>
