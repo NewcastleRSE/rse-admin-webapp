c02e91683291d0ade7462c5ebe7e0d92b9d9cfdd
Mark Turner <mark.turner@ncl.ac.uk>
2023-09-20 16:02:12 +0100

    popover for details on recovery chart

--- src/components/Finance/Recovery.vue
+++ src/components/Finance/Recovery.vue
@@ -13,11 +13,21 @@
             </div>
             <div class="relative my-4" aria-hidden="true">
                 <div class="overflow-hidden rounded-full bg-gray-200 h-8 relative">
-                    <div class="h-8 absolute bg-cyan-600 left-0" :style="{ width: `${billable}%` }" />
-                    <div class="h-8 absolute bg-yellow-400" :style="{ left: `${billable}%`, width: `${nonBillable}%` }" />
-                    <div class="h-8 absolute bg-red-400" :style="{ left: `${recorded}%`, width: `${missing}%` }" />
+                    <div ref="billableBar" v-on:mouseover="togglePopover(billableBar, 'billable')" v-on:mouseleave="togglePopover(billableBar, 'billable')" class="h-8 absolute bg-cyan-600 left-0" :style="{ width: `${billable}%` }" />
+                    <div ref="nonBillableBar" v-on:mouseover="togglePopover(nonBillableBar, 'nonBillable')" v-on:mouseleave="togglePopover(nonBillableBar, 'nonBillable')" class="h-8 absolute bg-yellow-400" :style="{ left: `${billable}%`, width: `${nonBillable}%` }" />
+                    <div ref="missingBar" v-on:mouseover="togglePopover(missingBar, 'missing')" v-on:mouseleave="togglePopover(missingBar, 'missing')" class="h-8 absolute bg-red-400" :style="{ left: `${recorded}%`, width: `${missing}%` }" />
                 </div>
                 <div class="h-12 w-0.5 -top-2 absolute bg-black" :style="{ left: `${progressThroughCapacity}%` }" />
+                <div ref="popoverRef" :class="{'hidden': !popoverShow, 'block': popoverShow}" :style="{ 'left': popoverX +'px', 'top': popoverY +'px' }" class="bg-blueGray-700 border-0 mb-3 fixed block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg">
+                    <div>
+                    <div class="text-white opacity-75 font-semibold p-3 mb-0 border-b border-solid border-slate-100 uppercase rounded-t-lg">
+                        {{ popoverTitle }}
+                    </div>
+                    <div class="text-white p-3">
+                        {{ popoverRatio }}% or {{ popoverDays }} days
+                    </div>
+                    </div>
+                </div>
             </div>
             <div class="flex px-4 py-3">
                 <ul class="py-2">
@@ -48,11 +58,25 @@
 <script setup>
 import { useRSEsStore, useTimesheetsStore } from '../../stores'
 import { DateTime } from 'luxon-business-days'
+import { createPopper } from '@popperjs/core'
+import { ref } from 'vue'
 import { currentFY } from '../../utils/dates'
 
 const timesheetStore = useTimesheetsStore(),
       rsesStore = useRSEsStore(),
-      dates = currentFY()
+      dates = currentFY(),
+      popoverShow = ref(false)
+
+const popover = ref(),
+      billableBar = ref(),
+      nonBillableBar = ref(),
+      missingBar = ref()
+
+let popoverTitle = '',
+    popoverRatio = 0,
+    popoverDays = 0,
+    popoverX = 0,
+    popoverY = 0
 
 const teamSummary = []
 
@@ -73,6 +97,51 @@ const recorded = ((totalRecorded / totalCapacity) * 100).toFixed(2),
 const workingDaysSoFar = DateTime.now().workingDiff(dates.startDate, 'days') * (teamSummary.length * averageProRata)
 
 const progressThroughCapacity = workingDaysSoFar > 0 ? ((workingDaysSoFar / totalCapacity) * 100).toFixed(2) : 0
+
+function togglePopover(reference, classification) {
+      if(popoverShow.value){
+        popoverShow.value = false
+      }
+      else {
+
+        const position = reference.getBoundingClientRect()
+
+        if(classification === 'billable') {
+            popoverTitle = 'Billable Time'
+            popoverRatio = billable
+            popoverDays = totalBillable
+            popoverX = position.x + (position.width / 2)
+            popoverY = position.y + (position.height * 1.5)
+            popoverShow.value = true
+        }
+        else if(classification === 'nonBillable') {
+            popoverTitle = 'Non-Billable Time'
+            popoverRatio = nonBillable
+            popoverDays = totalNonBillable
+            popoverX = position.x + (position.width / 2)
+            popoverY = position.y + (position.height * 1.5)
+            popoverShow.value = true
+        }
+        else if(classification === 'missing') {
+            popoverTitle = 'Missing Time'
+            popoverRatio = missing
+            popoverDays = totalMissing
+            popoverX = position.x + (position.width / 2)
+            popoverY = position.y + (position.height * 1.5)
+            popoverShow.value = true
+        }
+        
+        createPopper(reference, popover, {
+          placement: 'right'
+        })
+
+      }
+    }
+
+// console.log(totalCapacity)
+// console.log(totalCapacity * 0.85)
+// console.log(totalBillable)
+// console.log(totalNonBillable)
 
 const stats = [
   { name: 'Revenue', value: '£0', ratio: '0%' },
