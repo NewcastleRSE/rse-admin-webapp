ee30c9aca1ca9628795998bb919f5659166534a8
Mark Turner <mark.turner@ncl.ac.uk>
2023-09-29 16:47:47 +0100

    attempted adjustments for people being off during the year

--- src/components/Dashboard/TimeSummary.vue
+++ src/components/Dashboard/TimeSummary.vue
@@ -12,6 +12,7 @@
 <script setup>
 import { defineProps, toRefs } from 'vue'
 import { useTimesheetsStore } from '../../stores'
+import { Interval } from 'luxon'
 import { DateTime } from 'luxon-business-days'
 
 const props = defineProps({
@@ -26,7 +27,21 @@ let summary = timesheetStore.getRSESummary(props.rse)
 
 const capacity = props.rse.displayName === 'Mark Turner' ? 220 : rse.value.capacity
 
-const workingDaysSoFar = (DateTime.now().workingDiff(DateTime.fromISO(props.rse.capacityStart), 'days') * (capacity / 220)).toFixed(0)
+let currentCapacity
+
+props.rse.capacities.forEach(capacity => {
+
+    const period = Interval.fromDateTimes(DateTime.fromISO(capacity.start), DateTime.fromISO(capacity.end ? capacity.end : props.rse.capacityEnd))
+    
+    if(period.contains(DateTime.now())) {
+        currentCapacity = capacity
+    }
+})
+
+const workingDaysTarget = currentCapacity.capacity > 0 ? DateTime.now() : DateTime.fromISO(currentCapacity.start).minus({days: 1})
+const proRata =  capacity / (currentCapacity.capacity > 0 ? 220 : DateTime.fromISO(currentCapacity.start).diff(DateTime.fromISO(props.rse.capacityStart), ['days']).toObject().days)
+
+const workingDaysSoFar = (workingDaysTarget.workingDiff(DateTime.fromISO(props.rse.capacityStart), 'days') * proRata).toFixed(0)
 
 const progressThroughCapacity = workingDaysSoFar > 0 ? ((workingDaysSoFar / capacity) * 100).toFixed(2) : 0
 
@@ -37,6 +52,15 @@ const recorded = ((summary.recorded / summary.capacity) * 100).toFixed(2),
       accountedFor = (((summary.recorded + summary.leave) / summary.capacity) * 100).toFixed(2),
       missing = ((summary.missing / summary.capacity) * 100).toFixed(2)
 
-console.log(`${summary.name}, billable: ${summary.billable}, nonBillable: ${summary.nonBillable}, leave: ${summary.leave}, missing: ${summary.missing} of ${workingDaysSoFar}`)
+// if(summary.name === 'Frances Turner') {
+//     console.log(props.rse)
+//     console.log(currentCapacity)
+//     console.log(proRata)
+//     console.log(DateTime.fromISO(currentCapacity.start).diff(DateTime.fromISO(props.rse.capacityStart), ['days']).toObject())
+//     console.log(workingDaysSoFar)
+//     console.log(progressThroughCapacity)
+// }
+
+// console.log(`${summary.name}, billable: ${summary.billable}, nonBillable: ${summary.nonBillable}, leave: ${summary.leave}, missing: ${summary.missing} of ${workingDaysSoFar}`)
 
 </script>
