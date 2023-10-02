f1a96ffc89c31b122f62be4bae7e0226f995dd3b
Mark Turner <mark.turner@ncl.ac.uk>
2023-10-02 20:34:38 +0100

    initial bar chart for cloud costs

--- src/views/finances/Budget.vue
+++ src/views/finances/Budget.vue
@@ -1,7 +1,35 @@
 <template>
-    <div class="w-full mb-12 px-4">
-      <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
-        <h1 class="text-black">Budget</h1>
-      </div>
+  <div class="flex flex-wrap">
+    <div class="w-full xl:w-1/2 mb-12 xl:mb-0 px-4">
+      Expenses
     </div>
-  </template>
+    <div class="w-full xl:w-1/2 px-4">
+      <Cloud :data="cloud" />
+    </div>
+  </div>
+  <div class="flex flex-wrap mt-4">
+    <div class="w-full xl:w-1/2 mb-12 xl:mb-0 px-4">
+      Travel
+    </div>
+    <div class="w-full xl:w-1/2 px-4">
+      Other
+    </div>
+  </div>
+</template>
+<script setup>
+import Cloud from '@/components/Finance/Cloud.vue'
+import { useTransactionsStore } from '@/stores/transactions'
+
+import { currentFY } from '../../utils/dates'
+
+const dates = currentFY()
+
+const transactionsStore = useTransactionsStore()
+const transactions = transactionsStore.getByYear(dates.startDate.year)
+
+const expenses = transactions.filter(transaction => transaction.internalCategory === 'Expenses'),
+      cloud = transactions.filter(transaction => transaction.internalCategory === 'Cloud'),
+      travel = transactions.filter(transaction => transaction.internalCategory === 'Travel'),
+      other = transactions.filter(transaction => transaction.internalCategory === 'Other')
+
+</script>
