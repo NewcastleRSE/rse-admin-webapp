4e7ab29cdf5c08acfa9cfa8610e1958b71ee5e54
Mark Turner <mark.turner@ncl.ac.uk>
2023-10-05 22:59:13 +0100

    adjust utilisation rate to 80%

--- src/stores/capacities.js
+++ src/stores/capacities.js
@@ -70,12 +70,12 @@ export const useCapacitiesStore = defineStore('capacities', () => {
         let monthlyUtilisation = {
           date: startDate.toISODate(),
           capacityFTE: Math.round(capacitiesInPeriod.reduce(function (totalFTE, rse) { return totalFTE + rse.capacity }, 0)),
-          targetFTE: Math.round(capacitiesInPeriod.reduce(function (totalFTE, rse) { return totalFTE + rse.capacity }, 0) * 0.7),
+          targetFTE: Math.round(capacitiesInPeriod.reduce(function (totalFTE, rse) { return totalFTE + rse.capacity }, 0) * 0.8),
           actualFTE: Math.round(assignments.reduce(function (totalFTE, assignment) { return totalFTE + assignment.fte }, 0)),
           facilityFTE: Math.round(assignments.reduce(function (facilityFTE, assignment) { return assignment.project.costModel === 'Facility' ? (facilityFTE + assignment.fte) : facilityFTE + 0}, 0)),
           nonFacilityFTE: Math.round(assignments.reduce(function (nonFacilityFTE, assignment) { return assignment.project.costModel !== 'Facility' ? (nonFacilityFTE + assignment.fte) : nonFacilityFTE + 0 }, 0)),
           capacityDays: Math.round(capacitiesInPeriod.reduce(function (totalDays, rse) { return totalDays + ((220/12) * (rse.capacity/100)) }, 0)),
-          targetDays: Math.round(capacitiesInPeriod.reduce(function (totalDays, rse) { return totalDays + ((220/12) * (rse.capacity/100)) }, 0) * 0.7),
+          targetDays: Math.round(capacitiesInPeriod.reduce(function (totalDays, rse) { return totalDays + ((220/12) * (rse.capacity/100)) }, 0) * 0.8),
           actualDays: Math.round(assignments.reduce(function (totalDays, assignment) { return totalDays + ((220/12) * (assignment.fte/100)) }, 0)),
           facilityDays: Math.round(assignments.reduce(function (facilityDays, assignment) { return assignment.project.costModel === 'Facility' ? (facilityDays + ((220/12) * (assignment.fte/100))) : facilityDays + 0}, 0)),
           nonFacilityDays: Math.round(assignments.reduce(function (nonFacilityDays, assignment) { return assignment.project.costModel !== 'Facility' ? (nonFacilityDays + ((220/12) * (assignment.fte/100))) : nonFacilityDays + 0 }, 0))
