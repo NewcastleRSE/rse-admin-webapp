3f8a534a0b63b8eccb5adf150b64e8ae3d7ca98b
Mark Turner <mark.turner@ncl.ac.uk>
2024-08-24 07:45:33 +0100

    remove deprecated calendar logic

--- src/stores/calendar.js
+++ src/stores/calendar.js
@@ -1,119 +1,13 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import { currentFY } from '../utils/dates'
-import { DateTime } from 'luxon-business-days'
 import { fetchObject } from '../utils/orm'
 
 export const useCalendarStore = defineStore('calendar', () => {
-
-    const holidays = ref([])
-
-    const workingDays = ref([])
-
-    const isHoliday = function(inst) {
-        const closureDates = holidays.value.map(holiday => DateTime.fromISO(holiday.date))
-        return closureDates.includes(inst)
-    }
-      
-    const dt = DateTime.local();
-    dt.setupBusiness({ holidayMatchers: [isHoliday] })
-
-    function getHolidays() {
-        return holidays.value
-    }
-
-    function getWorkingDays() {
-        return workingDays.value
-    }
-
-    function getHolidaysByYear(year) {
-        return holidays.value.filter(holiday => holiday.date.split('-')[0] === year )
-    }
-
-    function getWorkingDaysByYear(year) {
-        return workingDays.value.filter(workingDay => workingDay.date.split('-')[0] === year )
-    }
-
-    function getHolidaysByAcademicYear(year) {
-        const dates = {
-            startDate: DateTime.fromISO(`${year}-08-01`),
-            endDate: DateTime.fromISO(`${(year + 1)}-07-31`)
-        }
-        return holidays.value.filter(holiday => dates.startDate <= DateTime.fromISO(holiday.date) && dates.endDate >= DateTime.fromISO(holiday.date))
-    }
-
-    function getWorkingDaysByAcademicYear(year) {
-        const dates = {
-            startDate: DateTime.fromISO(`${year}-08-01`),
-            endDate: DateTime.fromISO(`${(year + 1)}-07-31`)
-        }
-        return workingDays.value.filter(workingDay => dates.startDate <= DateTime.fromISO(workingDay.date) && dates.endDate >= DateTime.fromISO(workingDay.date))
-    }
 
     async function fetchCalendar (rse, year) {
         return await fetchObject('rse', `${rse.id}/calendar`, '*', { year: { $eq: year }})
     }
 
-    async function fetchHolidays () {
-        const ukBankHolidays = await fetch('https://www.gov.uk/bank-holidays.json').then((response) => response.json())
-        
-        const bankHolidays = ukBankHolidays['england-and-wales'].events,
-              closures = []
-
-        const christmases = bankHolidays.filter(holiday => holiday.title === 'Christmas Day')
-
-        christmases.forEach(christmas => {
-            const christmasBankHoliday = DateTime.fromISO(christmas.date)
-            const christmasEve = DateTime.fromISO(`${christmasBankHoliday.year}-12-24`)
-
-            for(let i=0; i<=7; i++) {
-                let closureDate = christmasEve.plus({days: i})
-                
-                if(closureDate.toISODate() !== christmasBankHoliday.toISODate()) {
-                    closures.push({
-                        title: 'University Closure',
-                        date: closureDate.toISODate(),
-                        notes: '',
-                        bunting: false
-                    })
-                } 
-            }
-
-            // If Christmas Eve is a Tuesday, the closure will start the day before
-            if(christmasEve.weekday === 2) {
-                closures.push({
-                    title: 'University Closure',
-                    date: christmasEve.minus({days: 1}).toISODate(),
-                    notes: '',
-                    bunting: false
-                })
-            }
-        })
-
-        holidays.value = [...bankHolidays, ...closures].sort((a,b) => DateTime.fromISO(a.date) - DateTime.fromISO(b.date))
-
-        const currentYear = currentFY()
-
-        let startPoint = currentYear.startDate
-
-        while (startPoint <= currentYear.endDate) {
-            
-            let holiday = holidays.value.find(holiday => holiday.date === startPoint.toISODate())
-
-            // If day is not a holiday or a weekend
-            if(!holiday && startPoint.weekday !== 5 && startPoint.weekday !== 6) {
-               workingDays.value.push(startPoint.toISODate())
-            }
-
-            startPoint = startPoint.plus({ days: 1 })
-        }
-    }
-
-    async function reset () {
-        holidays.value = []
-    }
-
-    return { holidays, workingDays, getHolidays, getWorkingDays, getHolidaysByYear, getWorkingDaysByYear, getHolidaysByAcademicYear, getWorkingDaysByAcademicYear, fetchHolidays, fetchCalendar, reset }
+    return { fetchCalendar }
 },
 {
     persist: true
