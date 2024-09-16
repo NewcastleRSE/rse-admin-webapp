7b30c263cbd95b069863c6130d9f774d3c6169b8
Mark Turner <mark.turner@ncl.ac.uk>
2024-09-16 09:34:14 +0900

    padding fix

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -15,7 +15,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="block w-full overflow-x-auto px-4">
+                <div class="block w-full overflow-x-auto px-4 pb-4">
                     <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         <li v-for="rse in rses" :key="rse.id" class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
                             <a :href="`/team/${(rse.displayName).replace(/\s+/g, '-').toLowerCase()}`" class="flex flex-1 flex-col p-8">
