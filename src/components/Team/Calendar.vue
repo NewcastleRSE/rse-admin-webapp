5c3c1413a788599d058c0d167b234eb52464b694
Mark Turner <mark.turner@ncl.ac.uk>
2024-09-24 11:24:00 +0100

    adjusted padding to fit new layout

--- src/components/Team/Calendar.vue
+++ src/components/Team/Calendar.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="bg-white">
       <div
-        class="mx-auto grid max-w-3xl grid-cols-1 gap-x-8 gap-y-16 px-4 py-16 sm:grid-cols-2 sm:px-6 xl:max-w-none xl:grid-cols-3 xl:px-8 2xl:grid-cols-4">
+        class="mx-auto grid max-w-3xl grid-cols-1 gap-x-8 gap-y-16 px-4 py-4 sm:grid-cols-2 sm:px-6 xl:max-w-none xl:grid-cols-3 xl:px-8 2xl:grid-cols-4">
         <section v-for="(month, monthIdx) in months" :key="month.name" class="text-center">
           <h2 class="text-sm font-semibold text-gray-900">{{ month.name }}</h2>
           <div class="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
