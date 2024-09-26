8c1f1a39b57a469f417874364837af0432319277
Mark Turner <mark.turner@ncl.ac.uk>
2024-09-26 08:43:54 +0100

    make allocation chart full width

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="flex flex-wrap">
-    <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+    <div class="w-full px-4">
       <capacity class="h-full" />
-    </div>
-    <div class="w-full xl:w-4/12 px-4">
-      <utilisation class="h-full" />
     </div>
   </div>
   <div class="flex flex-wrap mt-4">
     <div class="w-full xl:w-1/2 mb-12 xl:mb-0 px-4">
-      <project-status />
+      <utilisation class="h-full" />
     </div>
     <div class="w-full xl:w-1/2 px-4">
       <cost-recovery class="h-full" />
@@ -19,6 +16,5 @@
 <script setup>
 import Capacity from '@/components/Dashboard/Capacity.vue'
 import CostRecovery from '@/components/Dashboard/CostRecovery.vue'
-import ProjectStatus from '../components/Dashboard/ProjectStatus.vue'
 import Utilisation from '../components/Dashboard/Utilisation.vue'
 </script>
